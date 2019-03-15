b514f3796ac79511b6d9c417c31bb12d408d6144
Zino Onomiwo <zinoonomiwo@gmail.com>
2019-03-15 16:56:49 +0100

    work to gui

--- jobshop-gui/src/main.rs
+++ jobshop-gui/src/main.rs
@@ -1,87 +1,128 @@
-use std::io;
-use termion::raw::IntoRawMode;
-use termion::screen::AlternateScreen;
-use termion::event::Key;
+extern crate gtk;
+#[macro_use]
+extern crate relm;
+#[macro_use]
+extern crate relm_derive;
+#[macro_use]
+extern crate relm_attributes;
 
-use tui::Terminal;
-use tui::backend::TermionBackend;
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
-use utils::event::{ Event, Events };
+use relm_attributes::widget;
+use relm::DrawHandler;
+
+use relm::{Relm, Update, Widget};
+use gtk::prelude::*;
+use gtk::{Window, Inhibit, WindowType};
+use gtk::Orientation::Vertical;
 
 use disjunctgraph::{ Graph, LinkedGraph };
 use jobshop::problem::*;
 use jobshop::schedule::Schedule;
 
-mod utils;
 
-fn main() {
+#[derive(Msg)]
+pub enum Msg {
+    // …
+    Quit,
+}
 
-    let stdout = io::stdout().into_raw_mode().unwrap();
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend).unwrap();
-    let events = utils::event::Events::new();
+pub struct Model {
+    // …
+}
 
-    loop {
-        terminal.draw(|mut f| {            
-            let chunks = Layout::default().direction(Direction::Vertical)
-                .margin(1)
-                .constraints(
-                [ Constraint::Percentage(10), Constraint::Percentage(80), Constraint::Percentage(10)].as_ref()
-                )
-                .split(f.size());
-            Block::default()
-                .title("Block")
-                .borders(Borders::ALL)
-                .render(&mut f, chunks[0]);
-            Block::default()
-                .title("Block")
-                .borders(Borders::ALL)
-                .render(&mut f, chunks[1]);
-            Block::default()
-                .title("Block")
-                .borders(Borders::ALL)
-                .render(&mut f, chunks[2]);
-        }).unwrap();
+#[widget]
+impl Widget for Win {   
 
-        match events.next() {
-            Ok(Event::Input(key)) => {
-                if key == Key::Char('q') {
-                    break;
-                }
-            }
-            _ => {}
-        }
-
+    fn model() -> Model {
+        Model {}
     }
 
+    fn update(&mut self, event: Msg) {
+        
+    }
+
+    // Create the widgets.
+    widget! {
+        gtk::Window {
+            property_default_height: 650,
+            property_default_width: 1000,
+            title: "Window title",
+            gtk::Box {
+                orientation: Vertical,               
+                gtk::Label {
+                    // Bind the text property of this Label to the counter attribute
+                    // of the model.
+                    // Every time the counter attribute is updated, the text property
+                    // will be updated too.
+                    text: "Hello world",
+                },
+            },
+        }
+    }
+    /*fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        use gtk::Orientation::Vertical;
+        use gtk::{ Button, Label };
+        // GTK+ widgets are used normally within a `Widget`.
+        let window = Window::new(WindowType::Toplevel);
+        
+
+        let vbox = gtk::Box::new(Vertical, 0);
+
+        let counter_label = Label::new("Hello world");
+        vbox.add(&counter_label);
+        let counter_label = Label::new("Hello world 2");
+        vbox.add(&counter_label);
+        let counter_label = Label::new("Hello world 3");
+        vbox.add(&counter_label);
+
+        // Connect the signal `delete_event` to send the `Quit` message.
+        connect!(relm, window, connect_delete_event(_, _), return (Some(Msg::Quit), Inhibit(false)));
+        // There is also a `connect!()` macro for GTK+ events that do not need a
+        // value to be returned in the callback.
+
+        window.add(&vbox);
+        window.show_all();
+
+        Win {
+            model,
+            window: window,
+        }
+    }*/
+}
+
+fn main() {
+    Win::run(()).unwrap();
+}
+
+/*
+fn main() {
     
-    /*let path = "bench_test.txt";
+    let path = "bench_test.txt";
 
     let problem = Problem::read(path).unwrap();
     let graph = ProblemGraph::<LinkedGraph<ProblemNode>>::from(&problem).0;
 
     //println!("Problem statement: {:#?}", problem);
     //println!("Graph statement: {:?}", graph);    
-    let graph = graph.into_directed().unwrap();
-    println!("directed graph: {:?}", graph);
-    graph.force_critical_path();
-    let graph = graph.flip_edge(&5, &6).unwrap();
-    graph.force_critical_path();
-
-    let graph = graph.flip_edge(&2, &6).unwrap();
-    graph.force_critical_path();
-
-    let graph = graph.flip_edge(&3, &4).unwrap();
-    graph.force_critical_path();
-
+    
+    let graph = fix_graph(graph).unwrap();
+    let (span, _) = graph.force_critical_path();
     let schedule = Schedule::from_graph(problem, graph);
 
-    println!("Schedule: {:?}", schedule);
+    println!("Schedule: {:#?}", schedule);
+    println!("with span: {}", span);
 
     //println!("cyclic graph: {:?}", graph.flip_edge(&3, &9).unwrap());
 
     /*let mut f = File::create("test.dot").unwrap();
-    dot::render(&graph, &mut f).expect("Could not render to vizgraph");*/*/
+    dot::render(&graph, &mut f).expect("Could not render to vizgraph");*/
+}*/
+
+fn fix_graph(graph: LinkedGraph<ProblemNode>) -> Result<LinkedGraph<ProblemNode>, disjunctgraph::GraphError> {
+        graph.fix_disjunction(&4, &8)?
+            .fix_disjunction(&1, &7)?
+            .fix_disjunction(&6, &2)?
+            .fix_disjunction(&6, &5)?
+            .fix_disjunction(&2, &5)?
+            .fix_disjunction(&4, &3)?
+            .fix_disjunction(&8, &3)
 }
